3d8c463274fef27920d4e3ece3aab4d8d2cefa5d
David Robertson <davidjrobertson@live.co.uk>
2017-03-10 13:29:28 +0000

    more tests done

--- weekend_homework_start_point/pet_shop.rb
+++ weekend_homework_start_point/pet_shop.rb
@@ -10,6 +10,25 @@ def add_or_remove_cash(pet_shop, cash)
   pet_shop[:admin][:total_cash] += cash
 end
 
-def add_or_remove_cash__remove(pet_shop, cash)
-  pet_shop[:admin][:total_cash] -= cash
+def pets_sold(pet_shop)
+  pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount)
+  pet_shop[:admin][:pets_sold] += 2
+end
+
+def stock_count(pet_shop)
+  pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+  pets_by_breed = []
+  pets = pet_shop[:pets]
+  for pet in pets
+    if breed == pet[:breed]
+      pets_by_breed.push(pet)
+    end
+  end
+  return pets_by_breed
 end
