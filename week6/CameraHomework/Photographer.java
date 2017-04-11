4425bfd273db90222d128e4d984395c7ff5a8a96
David Robertson <davidjrobertson@live.co.uk>
2017-04-11 16:06:12 +0100

    started

---
+++ week6/CameraHomework/Photographer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Photographer{
+  private ArrayList<Camera> cameras; 
+
+  public Photographer(){
+    this.cameras = new ArrayList<Camera>();
+  }
+}
---
+++ week6/CameraHomework/PhotographerTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class PhotographerTest{
+  Camera camera;
+
+
+  @Before 
+  public void before() {
+    camera = new Camera;
+ 
+  }
