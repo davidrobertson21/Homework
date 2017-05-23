a94c7744131c66a6ed25410d8ab05149b3a68e66
David Robertson <davidjrobertson@live.co.uk>
2017-05-23 09:13:28 +0100

    uploaded

---
+++ week12/homework_start_point/controllers/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = new express.Router;
+
+var filmRouter = require("./films.js");
+
+router.use("/films", filmRouter);
+
+
+
+router.get('/', function(req, res) {
+  res.json({ data: 'Sup! Welcome to the Home Page'});
+});
+
+router.get('/about', function(req, res){
+  res.json({data: "About us"});
+})
+
+
+module.exports = router;
+
